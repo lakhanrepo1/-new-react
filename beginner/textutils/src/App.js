451c75faa20a9095b2d44f843089e45accd7fc65
lakhanrepo1 <lakhanshingare1998@gmail.com>
2026-02-19 21:56:28 +0530

    ReactJS Started learining-19-02-2026

---
+++ beginner/textutils/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name = "Lakhan";
+function App() {
+  return (
+   <>
+   <nav>
+   <li>Home</li>
+   <li>About</li>
+   <li>Contact</li>
+   </nav>
+   <div className="container">
+    <h1>Welcome {name}</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem corrupti qui officiis ipsum consequatur et accusantium mollitia soluta molestias pariatur veniam officia eaque numquam nulla exercitationem consectetur, assumenda eligendi unde?
+    </p>
+   </div>
+   </>
+  );
+}
+
+export default App;

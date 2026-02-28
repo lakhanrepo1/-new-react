805af4b438787bcdd6bbae6f61005005db072fe3
lakhanrepo1 <lakhanshingare1998@gmail.com>
2026-02-28 23:37:57 +0530

    UpTo Video:31

--- beginner/newsapp/src/components/NavBar.js
+++ beginner/newsapp/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 // import PropTypes from 'prop-types'
 
 export class NavBar extends Component {
@@ -7,9 +8,9 @@ export class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">
+            <Link className="navbar-brand" to="/">
               SquirrelNews
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -27,18 +28,18 @@ export class NavBar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
+                  <Link className="nav-link active" aria-current="page" to="/">
                     Home
-                  </a>
+                  </Link>
                 </li>
-                <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
-                  <li className="nav-item"><a className="nav-link" href="/business">business</a></li>
-                  <li className="nav-item"><a className="nav-link" href="/entertainment">entertainment</a></li>
-                  <li className="nav-item"><a className="nav-link" href="/general">general</a></li>
-                  <li className="nav-item"><a className="nav-link" href="/health">health</a></li>
-                  <li className="nav-item"><a className="nav-link" href="/science">science</a></li>
-                  <li className="nav-item"><a className="nav-link" href="/sports">sports</a></li>
-                  <li className="nav-item"><a className="nav-link" href="/technology">technology</a></li>
+                {/* <li className="nav-item"><a className="nav-link" href="/about">About</a></li> */}
+                  <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
               </ul>
             </div>
           </div>

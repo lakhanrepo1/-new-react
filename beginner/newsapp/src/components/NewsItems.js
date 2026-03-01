aad319599b12579b1a781607cc31ba8daf16eab4
lakhanrepo1 <lakhanshingare1998@gmail.com>
2026-03-01 15:04:58 +0530

    UpTo Video:32

--- beginner/newsapp/src/components/NewsItems.js
+++ beginner/newsapp/src/components/NewsItems.js
@@ -2,20 +2,34 @@ import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
-    let { title, description, urlToImage ,newsUrl} = this.props;
+    let { title, description, urlToImage, newsUrl, author, date, source } = this.props;
     return (
-
-      <div className="my-3" >
-        
-
-        <div className="card" >
+      <div className="my-3">
+        <div className="card">
+          <span className="position-absolute top-0  translate-middle badge rounded-pill bg-warning" style={{left:'85%',zIndex:'1'}}>
+                {source}
+              </span>
           <img src={urlToImage} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text" >
-              {description}...
+            <h5 className="card-title">
+              {title}
+              
+              ...
+            </h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              {" "}
+              <small className="text-muted">
+                By {!author ? "Unknown Author" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
             </p>
-            <a rel="noreferrer" href={newsUrl} className="btn btn-sm btn-dark" target="_blank">
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              className="btn btn-sm btn-dark"
+              target="_blank"
+            >
               Read More
             </a>
           </div>
